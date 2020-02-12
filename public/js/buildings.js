e7a90ae0313e52217871c225438684b8e4934287
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2020-02-12 17:38:45 +0530

    Reverse buildings and move text in middle

--- public/js/buildings.js
+++ public/js/buildings.js
@@ -6,17 +6,26 @@ const showData = buildings => {
 };
 
 const drawChart = buildings => {
-  const [width, height] = [400, 400];
+  // const [width, height] = [400, 400];
+  const chartSize = { width: 600, height: 400 };
+  const margin = { left: 100, right: 10, top: 10, bottom: 150 };
+
+  const width = chartSize.width - margin.left - margin.right;
+  const height = chartSize.height - margin.top - margin.bottom;
+
   const svg = d3
     .select("#chart-area")
     .append("svg")
-    .attr("height", height)
-    .attr("width", width);
+    .attr("height", chartSize.height)
+    .attr("width", chartSize.width);
+
+  buildingsG = svg.append("g");
+  buildingsG.attr("transform", `translate(${margin.left},${margin.right})`);
 
   const y = d3
     .scaleLinear()
     .domain([0, _.maxBy(buildings, "height").height])
-    .range([0, height]);
+    .range([height, 0]);
 
   const x = d3
     .scaleBand()
@@ -24,16 +33,48 @@ const drawChart = buildings => {
     .range([0, width])
     .padding(0.3);
 
-  const rects = svg.selectAll("rect").data(buildings);
+  const rects = buildingsG.selectAll("rect").data(buildings);
 
   const newRects = rects.enter();
   newRects
     .append("rect")
     .attr("x", b => x(b.name))
-    .attr("y", 0)
+    .attr("y", b => y(b.height))
     .attr("width", x.bandwidth)
-    .attr("height", b => y(b.height))
+    .attr("height", b => y(0) - y(b.height))
     .attr("fill", "grey");
+
+  buildingsG.append("text")
+    .attr("class", "axis-label")
+    .attr("x", width / 2)
+    .attr("y", height + 140)
+    .text("tall building");
+
+  buildingsG.append("text")
+    .attr("class", "axis-label")
+    .attr("transform", `rotate(-90)`)
+    .attr("x", -(height / 2))
+    .attr("y", -60)
+    .text("Height (m)");
+
+  const yAxis = d3.axisLeft(y).tickFormat(d => d + "m").ticks(3);
+
+  buildingsG.append("g")
+    .attr("class", "y-axis")
+    .call(yAxis);
+
+  const xAxis = d3.axisBottom(x);
+
+  const xAxisG = buildingsG.append("g")
+    .attr("class", "x-axis")
+    .attr("transform", `translate(0,${height})`)
+    .call(xAxis);
+
+  xAxisG.selectAll("text")
+    .attr("class", "building-names")
+    .attr("transform", "rotate(-90)")
+    .attr("x", 6)
+    .attr("y", 0);
 };
 
 const drawBuildings = buildings => {
