76ee7b09fe0f8d366d720f07d76c6e4272c26285
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2020-02-13 18:47:42 +0530

    Updation of axis values and removing companies from chart

--- public/js/companies.js
+++ public/js/companies.js
@@ -1,14 +1,26 @@
 
+const chartSize = { width: 800, height: 600 };
+const margin = { left: 100, right: 10, top: 10, bottom: 150 };
+
+const width = chartSize.width - margin.left - margin.right;
+const height = chartSize.height - margin.top - margin.bottom;
+
+const percentageFormat = d => `${d}%`;
+const kCroresFormat = d => `${d / 1000}k Cr ₹`;
+const rsFormat = d => d + "₹";
+
+const formats = {
+    MarketCap: kCroresFormat,
+    DivYld: percentageFormat,
+    QSales: kCroresFormat,
+    QNetProfit: kCroresFormat,
+    ROCE: percentageFormat,
+    CMP: rsFormat
+};
+
 const drawChart = companies => {
-    const chartSize = { width: 800, height: 600 };
-    const margin = { left: 100, right: 10, top: 10, bottom: 150 };
-
-    const width = chartSize.width - margin.left - margin.right;
-    const height = chartSize.height - margin.top - margin.bottom;
-
     const svg = d3
-        .select("#chart-area")
-        .append("svg")
+        .select("#chart-area svg")
         .attr("height", chartSize.height)
         .attr("width", chartSize.width);
 
@@ -28,9 +40,8 @@ const drawChart = companies => {
 
     const c = d3.scaleOrdinal(d3.schemeCategory10);
 
-    rects = companiesG.selectAll("rect").data(companies);
+    rects = companiesG.selectAll("rect").data(companies, c => c.Name);
 
-    console.log(companies[0].CMP);
     const newRects = rects.enter();
     newRects
         .append("rect")
@@ -41,19 +52,19 @@ const drawChart = companies => {
         .attr("fill", b => c(b.Name));
 
     companiesG.append("text")
-        .attr("class", "axis-label")
+        .attr("class", "x axis-label")
         .attr("x", width / 2)
         .attr("y", height + 140)
         .text("Companies");
 
     companiesG.append("text")
-        .attr("class", "axis-label")
+        .attr("class", "y axis-label")
         .attr("transform", `rotate(-90)`)
         .attr("x", -(height / 2))
         .attr("y", -60)
         .text("CMP");
 
-    const yAxis = d3.axisLeft(y).tickFormat(d => d + "₹").ticks(10);
+    const yAxis = d3.axisLeft(y).tickFormat(formats["CMP"]).ticks(10);
 
     companiesG.append("g")
         .attr("class", "y-axis")
@@ -73,6 +84,54 @@ const drawChart = companies => {
         .attr("y", 10);
 }
 
+const updateChart = function (companies, fieldName) {
+    const svg = d3.select("#chart-area svg");
+
+    svg.select(".y.axis-label").
+        text(fieldName);
+
+    const maxValue = _.get(_.maxBy(companies, fieldName), fieldName, 0);
+
+    y = d3
+        .scaleLinear()
+        .domain([0, maxValue])
+        .range([height, 0]);
+
+    const yAxis = d3.axisLeft(y)
+        .tickFormat(formats[fieldName])
+        .ticks(10);
+
+    svg.select(".y-axis")
+        .call(yAxis)
+
+    const t = d3.transition()
+        .duration(1000)
+        .ease(d3.easeLinear)
+
+    const x = d3
+        .scaleBand()
+        .domain(_.map(companies, "Name"))
+        .range([0, width])
+        .padding(0.3);
+
+    const xAxis = d3.axisBottom(x);
+
+    svg.select(".x-axis").call(xAxis)
+
+    svg.selectAll("rect")
+        .data(companies, c => c.Name)
+        .exit()
+        .remove()
+
+    svg.selectAll("rect")
+        .data(companies, c => c.Name)
+        .transition(t)
+        .attr("y", c => y(c[fieldName]))
+        .attr("x", c => x(c.Name))
+        .attr("height", c => y(0) - y(c[fieldName]))
+        .attr("width", x.bandwidth)
+}
+
 const drawCompanies = companies => {
     drawChart(companies);
 };
@@ -83,7 +142,14 @@ const parseCompany = ({ Name, ...numerics }) => {
 }
 
 const main = () => {
-    d3.csv("data/companies.csv", parseCompany).then(drawCompanies);
+    d3.csv("data/companies.csv", parseCompany).then(companies => {
+        drawCompanies(companies);
+
+        const fields = "CMP,PE,MarketCap,DivYld,QNetProfit,QSales,ROCE".split(",");
+        let step = 1;
+        setInterval(() => updateChart(companies, fields[step++ % fields.length]), 1000)
+        setInterval(() => companies.shift(), 3000);
+    });
 };
 
 window.onload = main;
